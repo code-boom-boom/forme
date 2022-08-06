7ebedbfb944db0bccbc093b07ca0049834badf63
lovecoding-mern <philip-acho@outlook.com>
2022-08-06 10:55:56 -0700

    protected route to default layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import Dashboard from './pages/Dashboard'
 import { AuthProvider } from './contexts/AuthContext'
+import HomePage from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/app" element={<DefaultLayout />}>
             <Route index element={<Dashboard />} />
           </Route>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
